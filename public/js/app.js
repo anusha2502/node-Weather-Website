2a73eb8249a790d5f856e34cb3ade0e109150293
Anusha Naik <meghamu@gmailcom>
2019-05-14 16:24:04 +0530

    setup for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,36 +1,30 @@
-console.log("Client side JavaScript!")
+console.log("Client side JavaScript!");
 
+const weatherForm = document.querySelector("form");
+const f1 = document.querySelector("form");
+const inp = document.querySelector("input");
+const para1 = document.querySelector("#msg1");
+const para2 = document.querySelector("#msg2");
 
+para1.textContent = "from JavaScript";
 
-const f1=document.querySelector('form')
-const inp=document.querySelector('input')
-const para1 = document.querySelector('#msg1')
-const para2 = document.querySelector('#msg2')
+f1.addEventListener("submit", e => {
+  e.preventDefault();
+  const loc = inp.value;
+  para1.textContent = "Loading Message";
+  para2.textContent = "";
 
-
-para1.textContent='from JavaScript'
-
-
-f1.addEventListener('submit',(e)=>{
-e.preventDefault()
-const loc=inp.value
-para1.textContent='Loading Message'
-para2.textContent=''
-
-fetch('http://localhost:3000/weather?address='+loc).then((response)=> {
-    response.json().then((data)=>
-    {
-        if(data.error){
-            para1.textContent=data.error
-            para2.textContent=''
-        }else{
-        para1.textContent=data.location
-        para2.textContent=data.fdata.summary
-       // para2.textContent=data.latitude
+  fetch("/weather?address=" + loc).then(response => {
+    response.json().then(data => {
+      if (data.error) {
+        para1.textContent = data.error;
+        para2.textContent = "";
+      } else {
+        para1.textContent = data.location;
+        para2.textContent = data.fdata.summary;
+        // para2.textContent=data.latitude
         //para2.textContent=data.longitude
-        }
-      
-    })
-})
-
-})
+      }
+    });
+  });
+});
